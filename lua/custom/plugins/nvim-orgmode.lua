cf6188d5d57393fdfdf6b14773a7bf2ca87056bb
till0r <till0r@users.noreply.github.com>
2024-09-21 07:00:07 +0200

    Basic kickstart vim configuration to my taste

---
+++ lua/custom/plugins/nvim-orgmode.lua
@@ -0,0 +1,20 @@
+return {
+  'nvim-orgmode/orgmode',
+  event = 'VeryLazy',
+  ft = { 'org' },
+  config = function()
+    -- Setup orgmode
+    require('orgmode').setup {
+      org_agenda_files = '~/orgfiles/**/*',
+      org_default_notes_file = '~/orgfiles/refile.org',
+      org_todo_keywords = { 'TODO', 'WAITING', '|', 'DONE' },
+    }
+
+    -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+    -- add ~org~ to ignore_install
+    -- require('nvim-treesitter.configs').setup({
+    --   ensure_installed = 'all',
+    --   ignore_install = { 'org' },
+    -- })
+  end,
+}
