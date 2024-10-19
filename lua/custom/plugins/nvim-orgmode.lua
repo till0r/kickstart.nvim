c1df1a8734412b69b72e544be5b5a3983158da7b
till0r <till0r@users.noreply.github.com>
2024-10-19 17:34:37 +0200

    Adds thoughts capture template

--- lua/custom/plugins/nvim-orgmode.lua
+++ lua/custom/plugins/nvim-orgmode.lua
@@ -8,6 +8,17 @@ return {
       org_agenda_files = '~/orgfiles/**/*',
       org_default_notes_file = '~/orgfiles/refile.org',
       org_todo_keywords = { 'TODO', 'WAITING', '|', 'DONE' },
+      org_capture_templates = {
+        t = {
+          description = 'Thoughts',
+          template = '%?',
+          datetree = {
+            treetype = 'month',
+            reversed = true,
+          },
+          target = '~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/thoughts.org',
+        },
+      },
     }
 
     -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
